44160afbf50df0f6ec0f5aefaecf8fdaec227f78
cm.lee <cm.lee@newtype.design>
2026-04-30 14:54:43 +0900

    리소스 모듈 수정

--- src/views/components/modules/Resource.vue
+++ src/views/components/modules/Resource.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import { inject, ref, watchEffect } from 'vue';
+import { computed, inject, ref, watchEffect } from 'vue';
 import MediaSelect from '@/views/components/form/MediaSelect.vue';
 import TextBox from '@/views/components/form/TextBox.vue';
 import ImageSelect from '@/views/components/form/ImageSelect.vue';
@@ -19,6 +19,7 @@ const data = ref();
 const filekey = inject('filekey') as string;
 const pickerIndex = ref(0);
 const showMediaPicker = ref(false);
+
 
 const addResource = (file: FileItem) => {
   // emit('update:modelValue', file);
@@ -44,12 +45,14 @@ watchEffect(() => {
 <template>
   <div resources :class="type">
     <div class="list">
-      <div class="col" v-for="(item, i) in data" :key="i">
+      <div class="col" v-for="(item, i) in data" :key="i" :class="{ invalid: data[i].type && !data[i].location }">
         <image-select :filekey="filekey" v-model="data[i].thumb"/>
         <div>
           <div class="d-flex">
             <DropSelect class="w-120 mr-2" v-model="data[i].type" placeholder="파일 구분" :list="['Image','Video','PDF','etc']"/>
-            <b-button variant="default" @click="pickerIndex = i; showMediaPicker = true">{{ data[i].location ? 'Change' : 'Upload' }} File</b-button>
+            <b-button class="btn-upload" variant="default" @click="pickerIndex = i; showMediaPicker = true">{{ data[i].location ? 'Change' : 'Upload'
+              }} File
+            </b-button>
           </div>
           <text-input placeholder="파일명을 입력해 주세요" v-model="data[i].filename"/>
           <text-input placeholder="파일 사이즈를 단위와 함께 입력해 주세요." v-model="data[i].size"/>
